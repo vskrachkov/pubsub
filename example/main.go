faaf2d20ace3aa64a55219eeb64c4468fcc119d3
vs <vskrachkov@gmail.com>
2019-11-03 23:01:54 +0200

    new: subscription_manager.go

---
+++ example/main.go
@@ -0,0 +1,54 @@
+package main
+
+import (
+	"fmt"
+	"github.com/go-redis/redis"
+	log "github.com/sirupsen/logrus"
+	"github.com/vskrachkov/pubsub/src"
+	"time"
+)
+
+func init() {
+	log.SetFormatter(&log.TextFormatter{FullTimestamp: true})
+	log.SetLevel(log.InfoLevel)
+}
+
+func main() {
+	log.Info("test log")
+
+	redisClient := redis.NewClient(&redis.Options{
+		Addr:     "localhost:6379",
+		Password: "", // no password set
+		DB:       0,  // use default DB
+	})
+
+	sm := src.NewSubscriptionManager(redisClient)
+	sm.Subscribe("ch:1", "a.*", func(msg *redis.Message) {
+		log.Info(
+			fmt.Sprintf(
+				"['%v' '%v']: %v",
+				msg.Channel,
+				msg.Pattern,
+				msg.Payload,
+			),
+		)
+	})
+
+	go func() {
+		for i := 1; i <= 10; i++ {
+			// Publish a message.
+			time.Sleep(time.Second)
+			msg := fmt.Sprintf("hello %v", i)
+			log.Info("publish message: ", msg)
+			err := redisClient.Publish("a.1", msg).Err()
+			if err != nil {
+				panic(err)
+			}
+		}
+	}()
+
+	for {
+		time.Sleep(time.Second)
+	}
+
+}
---
+++ src/subscription_manager.go
@@ -0,0 +1,83 @@
+package src
+
+import (
+	"github.com/go-redis/redis"
+	log "github.com/sirupsen/logrus"
+)
+
+type Consumer = func(message *redis.Message)
+
+type SubscriptionManager struct {
+	redisClient    *redis.Client
+	pubSubs        map[string]*redis.PubSub
+	consumers      map[string]*[]Consumer
+	clientChannels map[string]*[]string
+}
+
+func NewSubscriptionManager(redisClient *redis.Client) *SubscriptionManager {
+	return &SubscriptionManager{
+		redisClient:    redisClient,
+		pubSubs:        map[string]*redis.PubSub{},
+		consumers:      map[string]*[]Consumer{},
+		clientChannels: map[string]*[]string{},
+	}
+}
+
+func (sm *SubscriptionManager) Subscribe(
+	clientChannel string,
+	pattern string,
+	consumer Consumer,
+) {
+	log.Info("subscribing to the pattern: ", pattern)
+	sm.updateConsumers(pattern, consumer)
+	sm.updatestrings(pattern, clientChannel)
+	ch := sm.subscribe(pattern) // Go channel which receives messages
+	go func() {
+		for msg := range ch {
+			consumer(msg)
+		}
+	}()
+}
+
+func (sm SubscriptionManager) updateConsumers(
+	pattern string,
+	consumer Consumer,
+) {
+	if sm.consumers[pattern] != nil {
+		cons := sm.consumers[pattern]
+		*cons = append(*cons, consumer)
+	} else {
+		cons := make([]Consumer, 8)
+		cons = append(cons, consumer)
+		sm.consumers[pattern] = &cons
+	}
+}
+
+func (sm SubscriptionManager) updatestrings(
+	pattern string,
+	clientChannel string,
+) {
+	if sm.clientChannels[pattern] != nil {
+		chs := sm.clientChannels[pattern]
+		*chs = append(*chs, clientChannel)
+	} else {
+		chs := make([]string, 8)
+		chs = append(chs, clientChannel)
+		sm.clientChannels[pattern] = &chs
+	}
+}
+
+func (sm SubscriptionManager) subscribe(
+	pattern string,
+) <-chan *redis.Message {
+	pubsub := sm.pubSubs[pattern]
+	if pubsub == nil {
+		pubsub := sm.redisClient.PSubscribe(pattern)
+		_, err := pubsub.Receive() // Wait for confirmation that subscription is created
+		if err != nil {
+			panic(err)
+		}
+		sm.pubSubs[pattern] = pubsub
+	}
+	return sm.pubSubs[pattern].Channel()
+}
